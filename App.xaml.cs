8c0d359f2bfa4caa2ef057b05b7314ea667f2593
Oldfoollearnc <121083652+Oldfoollearnc@users.noreply.github.com>
2026-06-30 10:18:59 +0800

    Add App.xaml.cs

---
+++ App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AIDailyNews;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
+
